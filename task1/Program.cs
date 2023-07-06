de0bfd002068d4ed0cc7fff06a28b5f3d3e0d8c3
Igor_Potapoff <igor.potapoff@yandex.ru>
2023-07-06 11:06:40 +0300

    Homework7

---
+++ task1/Program.cs
@@ -0,0 +1,69 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+
+Console.Clear();
+
+double[,] CreateArray(int row, int col)
+{
+
+    Random rand = new Random();
+    double[,] array = new double[row, col];
+
+    for (int i = 0; i < row; i++)
+    {
+
+        for (int j = 0; j < col; j++)
+        {
+
+            array[i, j] = Math.Round(rand.Next(-1000, 1001)*0.1, 1);
+        }
+
+    }
+
+    return array;
+
+}
+
+
+void PrintArray(double[,] array)
+{
+
+    int row = array.GetLength(0);
+    int col = array.GetLength(1);
+
+    for (int i = 0; i < row; i++)
+    {
+
+        for (int j = 0; j < col; j++)
+        {
+            if (array[i,j]%1==0){
+
+            Console.Write($"{array[i, j]:F0}\t");
+
+            }
+
+            else
+
+            Console.Write($"{array[i, j]:F2}\t");
+
+        }
+
+        Console.WriteLine();
+
+    }
+
+}
+
+double[,] array = CreateArray(3,4);
+
+PrintArray(array);
+
+Console.WriteLine();
+
+
+
---
+++ task3/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+
+
+Console.Write("Введите размерность m массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите размерность n массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] randomArray = new int[m,n];
+
+void CreateArray(int m, int n)
+{
+int i,j;
+Random rand = new Random();
+for (i = 0; i < m; i++)
+{
+Console.WriteLine();
+for (j = 0; j < n; j++)
+{
+randomArray[i,j] = rand.Next(1,9);
+Console.Write($"{randomArray[i,j]} ");
+}
+Console.WriteLine();
+}
+}
+
+void FindAverage(int m, int n)
+{
+Console.Write("Среднее арифметическое каждого столбца: ");
+int i,j;
+Random rand = new Random();
+for (j = 0; j < n; j++)
+{
+double sum = 0;
+for (i = 0; i < m; i++)
+{
+sum = sum + randomArray[i,j];
+}
+Console.Write($"{sum/(i):F1}; ");
+}
+}
+CreateArray(m,n);
+FindAverage(m,n);
